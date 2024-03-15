1994eb8c310dc014d63fdfb4aa02dcd20b0cb90c
Gabriel <gabrielferv95@gmail.com>
2024-03-14 21:00:26 -0500

    discoveryService

---
+++ registerService/src/test/java/com/microservicios3/registerService/RegisterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios3.registerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
